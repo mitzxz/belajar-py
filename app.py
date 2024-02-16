dd482f2af1e6220e960ef58166ab5d661d3c54b3
aryo <serchostz@gmail.com>
2024-02-16 10:07:06 +0700

    First Project Push

---
+++ app.py
@@ -0,0 +1,9 @@
+# Meminta input dari pengguna
+angka1 = float(input("Masukkan angka pertama: "))
+angka2 = float(input("Masukkan angka kedua: "))
+
+# Menjumlahkan dua angka
+hasil = angka1 + angka2
+
+# Menampilkan hasil penjumlahan
+print("Hasil dari", angka1, "+", angka2, "adalah", hasil)
